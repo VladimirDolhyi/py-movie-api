9f3b446dfc4767fad50ea9353d43681510ae8cf6
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-11-29 17:55:44 +0200

    fix Solution

--- movie_api/settings.py
+++ movie_api/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r_kbdgzf&%1986iz$mtgu!-tzo^0u_8p=1cl7m%k*$sf)n#*b("
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
